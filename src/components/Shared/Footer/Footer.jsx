886e39a7df7eeee6251def87db7ebc38a0e546ed
e <jannatul0040@gmail.com>
2023-11-26 00:30:13 +0600

    updated footer section

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,44 +1,57 @@
-
+import { FaFacebookF, FaGoogle, FaPinterest, FaTwitter, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <footer className="footer p-10 bg-base-200 text-base-content">
+<div>
+<footer className="footer p-10 bg-[#12151a] text-white">
 
 <nav>
-    <header className="footer-title">Services</header> 
-    <a className="link link-hover">Branding</a>
-    <a className="link link-hover">Design</a>
-    <a className="link link-hover">Marketing</a>
-    <a className="link link-hover">Advertisement</a>
+    <header className="footer-title border-b w-full py-3">CONTACT US</header> 
+    <a className="link link-hover">123 ABS Street, Uni 21, Bangladesh</a>
+    <a className="link link-hover">+88 123456789</a>
+    <a className="link link-hover">Mon - Fri: 08:00 - 22:00</a>
+    <a className="link link-hover">Sat - Sun: 10:00 - 23:00</a>
   </nav> 
   <nav>
-    <header className="footer-title">Company</header> 
+    <header className="footer-title border-b w-full py-3">Company</header> 
     <a className="link link-hover">About us</a>
     <a className="link link-hover">Contact</a>
     <a className="link link-hover">Jobs</a>
     <a className="link link-hover">Press kit</a>
   </nav> 
   <nav>
-    <header className="footer-title">Legal</header> 
-    <a className="link link-hover">Terms of use</a>
-    <a className="link link-hover">Privacy policy</a>
-    <a className="link link-hover">Cookie policy</a>
-  </nav> 
+    <header className="footer-title border-b w-full py-3">Services</header> 
+    <a className="link link-hover">Branding</a>
+    <a className="link link-hover">Design</a>
+    <a className="link link-hover">Marketing</a>
+    <a className="link link-hover">Advertisement</a>
+  </nav>  
   <form>
-    <header className="footer-title">Newsletter</header> 
+    <header className="footer-title border-b w-full py-3">Newsletter</header> 
     <fieldset className="form-control w-80">
-      <label className="label">
-        <span className="label-text">Enter your email address</span>
-      </label> 
-      <div className="join">
-        <input type="text" placeholder="username@site.com" className="input input-bordered join-item" /> 
-        <button className="btn btn-primary join-item">Subscribe</button>
+      <div className="">
+        <input type="text" placeholder="Enter your email address" className="rounded-sm input input-bordered join-item" /> 
+        <button className="btn border-none rounded-sm ml-2 bg-[#dc0003] text-white">Subscribe</button>
       </div>
     </fieldset>
   </form>
 
   
 </footer>
+<div className="footer items-center p-4 bg-black text-white">
+  <aside className="items-center grid-flow-col"> 
+    <p>Copyright © 2023 - All right reserved</p>
+  </aside> 
+  <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+    <a><FaFacebookF className="bg-[#dc0003] h-8 w-8 p-2 rounded-full"></FaFacebookF></a>
+    <a><FaYoutube className="bg-[#dc0003] h-8 w-8 p-2 rounded-full"></FaYoutube></a>
+    <a><FaGoogle className="bg-[#dc0003] h-8 w-8 p-2 rounded-full"></FaGoogle></a>
+<a><FaPinterest className="bg-[#dc0003] h-8 w-8 p-2 rounded-full"></FaPinterest></a>
+<a className="bg-[#dc0003] h-8 w-8 p-2 rounded-full"><FaTwitter></FaTwitter></a>
+  </nav>
+</div>
+<div className="h-2 bg-[#dc0003]"></div>
+</div>
     );
 };
 
