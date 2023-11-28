113bcbf1049ce33ebd6a65dc48e8df6b490120b2
e <jannatul0040@gmail.com>
2023-11-29 00:52:56 +0600

    ake admin and deleted user from ui

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -2,17 +2,60 @@ import { useQuery } from "@tanstack/react-query";
 import SectionTitle from "../../../components/Shared/SectionTitle/SectionTitle";
 import useAxiosSecure from "../../../hook/useAxiosSecure";
 import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 
 const AllUsers = () => {
     const axiosSecure = useAxiosSecure();
-    const { data: users = [] } = useQuery({
+    const { data: users = [],refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const result = await axiosSecure.get('/users');
             return result.data;
         }
     })
+
+    //Make Admin 
+    const handleMakeAdmin = user =>{
+        axiosSecure.patch(`/users/admin/${user._id}`)
+        .then(res => {
+            if (res.data.modifiedCount > 0) {
+                refetch();
+                Swal.fire({
+                    title: "Congratulations!",
+                    text: `${user.name} is an Admin Now!`,
+                    icon: "success"
+                  });
+            }
+        })
+
+    }
+    const handleDeleteUser =user =>{
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.delete(`/users/${user._id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "User has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
     return (
         <div className="p-8">
             <SectionTitle heading='All Users'>
@@ -37,25 +80,28 @@ const AllUsers = () => {
                     </thead>
                     <tbody>
                         {
-                            users.map((users,index) => <tr key={users._id}>
+                            users.map((user,index) => <tr key={user._id}>
                                 <th>{index + 1}</th>
                                 <td>
                                     <div className="flex items-center gap-3">
                                         <div className="avatar">
                                             <div className="rounded-full w-12 h-12">
-                                                <img src={users.photo} />
+                                                <img src={user.photo} />
                                             </div>
                                         </div>
                                         
                                     </div>
                                 </td>
-                                <td>{users.name}</td>
-                                <td>{users.email}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
                                 <th>
-                                    <button className="btn btn-ghost btn-sm bg-[#B91C1C] text-white">Make Admin</button>
+                                    { user.role==='admin' ? 'Admin' :<button 
+                                    onClick={() =>handleMakeAdmin(user)}
+                                    className="btn btn-ghost btn-sm bg-[#B91C1C] text-white">Make Admin</button>}
                                 </th>
                                 <td>
                                     <button
+                                    onClick={() => handleDeleteUser(user)}
                                         className="btn btn-ghost btn-sm bg-[#B91C1C]">
                                         <FaTrashAlt className="text-white"></FaTrashAlt>
                                     </button>
