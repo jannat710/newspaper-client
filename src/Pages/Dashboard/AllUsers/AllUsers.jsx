f9b53594ff6f901bf39474d22b0511d1fa656cdf
e <jannatul0040@gmail.com>
2023-11-28 22:37:30 +0600

    displayed all user in dashboard

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,23 +1,72 @@
 import { useQuery } from "@tanstack/react-query";
 import SectionTitle from "../../../components/Shared/SectionTitle/SectionTitle";
 import useAxiosSecure from "../../../hook/useAxiosSecure";
+import { FaTrashAlt } from "react-icons/fa";
 
 
 const AllUsers = () => {
     const axiosSecure = useAxiosSecure();
-    const {data: data =[]}=useQuery({
-        queryKey:['users'],
-        queryFn:async () =>{
+    const { data: users = [] } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
             const result = await axiosSecure.get('/users');
             return result.data;
         }
     })
     return (
-        <div>
+        <div className="p-8">
             <SectionTitle heading='All Users'>
             </SectionTitle>
             <div>
-                <h2 className="text-3xl">Total Users: {data.length}</h2>
+                <h2 className="text-3xl py-4">Total Users: {users.length}</h2>
+                
+            </div>
+
+            <div className="overflow-x-auto">
+                <table className="table w-full mx-auto">
+                    {/* head */}
+                    <thead className="bg-black text-white">
+                        <tr>
+                            <th></th>
+                            <th>Picture</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((users,index) => <tr key={users._id}>
+                                <th>{index + 1}</th>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="rounded-full w-12 h-12">
+                                                <img src={users.photo} />
+                                            </div>
+                                        </div>
+                                        
+                                    </div>
+                                </td>
+                                <td>{users.name}</td>
+                                <td>{users.email}</td>
+                                <th>
+                                    <button className="btn btn-ghost btn-sm bg-[#B91C1C] text-white">Make Admin</button>
+                                </th>
+                                <td>
+                                    <button
+                                        className="btn btn-ghost btn-sm bg-[#B91C1C]">
+                                        <FaTrashAlt className="text-white"></FaTrashAlt>
+                                    </button>
+                                </td>
+                            </tr>)
+                        }
+
+                    </tbody>
+
+
+                </table>
             </div>
 
         </div>
--- src/components/Shared/SectionTitle/SectionTitle.jsx
+++ src/components/Shared/SectionTitle/SectionTitle.jsx
@@ -3,7 +3,7 @@
 const SectionTitle = ({heading}) => {
     return (
         <div>
-             <div className="text-center px-8 my-8 mx-auto">
+             <div className="text-center mx-auto">
 
             <h3 className="text-[#dc0003] text-sm md:text-xl lg:text-3xl  uppercase py-4 divider  divider-end divider-neutral">{heading}</h3>
         </div>
